ae4228abc480fc511f8c693ab0c50cca842bf9b3
Tim Pequignot <tim@peq.me>
2021-10-17 21:46:57 -0500

    Use std/http (#4)
    * Add std/http dependencies
    
    * Replace depreciated event listener with std/http
    
    * Update README
    
    * Error response Fixes:  (correct content type header, remove error markup)

--- errorResponse.ts
+++ errorResponse.ts
@@ -1,13 +1,10 @@
 export function errorResponse(errorCode?:number, errorText = ""):Response{
-  return new Response(`
-    <body style="text-align: center; font-family: Avenir, Helvetica, Arial, sans-serif">
-      <p style="color:red;">${errorText}</p>
-    </body>
-  `,
+  return new Response(errorText,
   {
     status: errorCode ? errorCode : 400,
+    statusText: errorText,
     headers: {
-      "content-type": "text/html; charset=UTF-8",
+      "content-type": "text/plain; charset=UTF-8",
     },
     
   });
